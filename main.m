b354130e0e2f5b3f5f17bf3ca1260fd318489b71
shindelr <129821483+shindelr@users.noreply.github.com>
2024-04-05 08:54:57 -0700

    Initial commit of the decompressed files in the PIV demo that Prof. Wilson sent.

---
+++ main.m
@@ -0,0 +1,75 @@
+% Minimal PIV calculation for testing and development.  Uses a sample
+% Hopkins 2023 ROXSI image-pair.
+%
+clear
+
+doprofile=0;  % set to '1' to use the matlab code profiler
+
+% load the two sample image frames into memory
+im1=imread('data/im1.jpg');
+im2=imread('data/im2.jpg');
+
+% set window sizes.  The algorithm will start by analyzing the frame-pairs
+% with an analysis window of size pass_sizes(1), then refines its estimate
+% with a smaller window (pass_sizes(2)), and so on.
+pivwin=16;  % 16pix is a final window size that worked well for ROXSI 2023
+log2pivwin=log2(pivwin);
+if(log2pivwin-round(log2pivwin)~=0)
+  error('pivwin must be a factor of 2')
+end
+pass_sizes=2.^[6:-1:log2pivwin]';  % step-down the window sizes in powers of two
+
+% make the analysis window sizes 2d (as required for the matpiv code), and
+% repeat the last window iteration (also required)
+pass_sizes=[pass_sizes pass_sizes];
+pass_sizes=[pass_sizes;pass_sizes(end,:)];
+
+% other input params for matpiv
+dt=1;%1/40  % frame time step in seconds.  Set to 1 to get 'pixels per frame' velocity
+overlap=0.5;  % fraction of window overlap
+validvec=3;  % threshold for vector validation during multipass
+
+% run the PIV analysis
+if(doprofile)
+  profile on  % optional, initiate the matlab profiler
+end
+[x,y,u,v,SnR,Pkh]=multipassx(im1,im2,pass_sizes,...
+                             dt,overlap,validvec);
+if(doprofile)
+  profile report  % optional
+end
+
+% save original results prior to quality control...
+uraw=u;
+vraw=v;
+
+% snrfilt: reject data with too-low signal to noise level
+snrthresh=1.3;
+ibad=find(SnR<snrthresh);
+u(ibad)=nan;
+v(ibad)=nan;
+
+% peakfilt: reject data for which the correlation peak was too low
+pkhthresh=0.3;
+ibad=find(Pkh<pkhthresh);
+u(ibad)=nan;
+v(ibad)=nan;
+
+% globfilt: reject data that disagree strongly with its neighbors in a local
+% window
+[u,v]=globfilt(x,y,u,v,3);
+
+% plot the results
+clf
+pause(.5)
+subplot(211)
+pcolor(u),sf
+title('u [pixels/frame]')
+subplot(212)
+pcolor(v),sf
+title('v [pixels/frame]')
+for i=1:2
+  subplot(2,1,i)
+  axis equal tight
+  ylim([0 200])
+end
---
+++ multipassx.m
@@ -0,0 +1,55 @@
+function [x,y,u,v,SnR,Pkh]=multipassx(A,B,wins,Dt,overlap,sensit)
+
+A=double(A);
+B=double(B);
+
+[sy,sx]=size(A);
+iter=size(wins,1);
+
+% Initial passes are for removing large-scale displacements.  Initialize
+% displacements (datax,datay) to zero
+datax=zeros(floor(sy/(wins(1,1)*(1-overlap))),floor(sx/(wins(1,2)*(1-overlap))));
+datay=zeros(floor(sy/(wins(1,1)*(1-overlap))),floor(sx/(wins(1,2)*(1-overlap))));
+for i=1:iter-1
+  disp(['iter ' num2str(i) ' of ' num2str(iter)])
+
+  % PIV
+  [x,y,datax,datay]=firstpass(A,B,wins(i,:),overlap,datax,datay);
+
+  % validation.  TODO, simplify these codes!
+  % [datax,datay]=globfilt(x,y,datax,datay,3);
+  [datax,datay]=localfilt(x,y,datax,datay,sensit,'median',3,[]);
+  [datax,datay]=naninterp(datax,datay,'linear',[],x,y);
+  datax=floor(datax);
+  datay=floor(datay);
+
+  % expand the velocity data to twice the original size
+  if(i~=iter-1)
+    if wins(i,1)~=wins(i+1,1)
+      X=(1:((1-overlap)*2*wins(i+1,1)):sx-2*wins(i+1,1)+1) + wins(i+1,1);
+      XI=(1:((1-overlap)*wins(i+1,1)):sx-wins(i+1,1)+1)+(wins(i+1,1))/2;
+    else
+      XI=(1:((1-overlap)*wins(i+1,1)):sx-wins(i+1,1)+1)+(wins(i+1,1))/2;
+      X=XI;
+    end
+    if wins(i,2)~=wins(i+1,2)
+      Y=(1:((1-overlap)*2*wins(i+1,2)):sy-2*wins(i+1,2)+1) + wins(i+1,2);
+      YI=(1:((1-overlap)*wins(i+1,2)):sy-wins(i+1,2)+1)+(wins(i+1,2))/2;
+    else
+      YI=(1:((1-overlap)*wins(i+1,2)):sy-wins(i+1,2)+1)+(wins(i+1,2))/2;
+      Y=YI; 
+    end
+    datax=round(interp2(X,Y',datax,XI,YI'));
+    datay=round(interp2(X,Y',datay,XI,YI'));
+    [datax,datay]=naninterp(datax,datay,'linear',[],...
+                            repmat(XI,size(datax,1),1),...
+                            repmat(YI',1,size(datax,2)));  % TODO simplify!
+    datax=round(datax);
+    datay=round(datay);
+  end
+
+end
+
+% Final pass gives displacement to subpixel accuracy
+disp('Final iteration')
+[x,y,u,v,SnR,Pkh]=finalpass(A,B,wins(end,:),overlap,round(datax),round(datay),Dt);
