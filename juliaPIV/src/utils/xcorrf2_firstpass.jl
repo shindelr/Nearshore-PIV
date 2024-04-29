e064ba37912b9ed9e3ab34fcd7d67cc7008167c3
shindelr <129821483+shindelr@users.noreply.github.com>
2024-04-29 10:13:05 -0400

    Started the firstpass version of xcorrf2

---
+++ juliaPIV/src/utils/xcorrf2_firstpass.jl
@@ -0,0 +1,10 @@
+#   c = xcorrf2(a,b)
+#    Two-dimensional cross-correlation using Fourier transforms.
+#        XCORRF2(A,B) computes the crosscorrelation of matrices A and B.
+#        XCORRF2(A) is the autocorrelation function.
+#        This routine is functionally equivalent to xcorr2 but usually faster.
+#        See also XCORR2.
+
+#        Author(s): R. Johnson
+#        $Revision: 1.0 $  $Date: 1995/11/27 $
+
