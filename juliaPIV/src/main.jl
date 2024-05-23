aa4f440d85def58bad95e899b5c88ea96d2c5858
shindelr <129821483+shindelr@users.noreply.github.com>
2024-05-23 12:00:50 -0700

    Got some stuff done in multipassx and firstpass x. Prettied up main and some docstrings

--- juliaPIV/src/main.jl
+++ juliaPIV/src/main.jl
@@ -3,21 +3,20 @@ using FFTW
 # PASS FUNCTIONS
 """
 ### multipassx
-
-\n**:params:**\n
-A: Matrix containing image data of first frame.\n
-B: Matrix containing image data of second frame.\n
-wins: 2D matrix of ints containing sub-window pixel sizes for each pass.\n
-Dt: Frame time step in seconds (int). Pass 1 for 'pixels per frame velocity'.\n
-overlap: Fraction of window overlap. Int.\n
-sensit: Threshold for vector validation. Int. \n
-\n**:returns:**\n
-x: \n
-y: \n
-u: \n
-v: \n
-SnR: Ratio representing signal-to-noise.\n
-Pkh: Peak height for use in validation of vector field?\n
+    \n**:params:**\n
+    A: Matrix containing image data of first frame.\n
+    B: Matrix containing image data of second frame.\n
+    wins: 2D matrix of ints containing sub-window pixel sizes for each pass.\n
+    Dt: Frame time step in seconds (int). Pass 1 for 'pixels per frame velocity'.\n
+    overlap: Fraction of window overlap. Int.\n
+    sensit: Threshold for vector validation. Int. \n
+    \n**:returns:**\n
+    x: \n
+    y: \n
+    u: \n
+    v: \n
+    SnR: Ratio representing signal-to-noise.\n
+    Pkh: Peak height for use in validation of vector field?\n
 """
 function multipassx(A, B, wins, Dt, overlap, sensit)
     # Convert A, B to floats
@@ -34,7 +33,16 @@ function multipassx(A, B, wins, Dt, overlap, sensit)
     datax = zeros(eltype(A), (data_dim_1, data_dim_2))
     datay = zeros(eltype(A), (data_dim_1, data_dim_2))
 
+    for i in 1:iter-1
+        println("Iter ", i, " of ", iter )
+        # PIV proper
+        x, y, datax, datay = firstpass(A, B, wins[i, :], overlap, datax, datay)
 
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # !!!!!!!!!! Complete multipassx below here !!!!!!
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    end
 
     # Dummy values
     x=0; y=0; u=0; v=0; SnR=0; Pkh=0;
@@ -44,13 +52,48 @@ end
 
 
 """
-TODO:
+### firstpass
+*Note: First pass is a misnomer for this function, as it's called N-1 times
+depending on the size of the given windows. Consider renaming*\n\n
+    Set up matrix indices along the image frame according to the desired overlap and \
+    window sizes. Calls **xcorrf2** which finally uses FFTs to calculate cross \
+    correlation.
+    \n**:params:**\n
+    A: Matrix containing image data of first frame.\n
+    B: Matrix containing image data of second frame.\n
+    N: Pair of Float64 representing sub-window pixel sizes for the pass.\n
+    overlap: Fraction of window overlap. Int.\n
+    idx: Matrix of same type as A, containing data displacement information.\n
+    idy: Matrix of same type as A, containing data displacement information.\n
+    \n**:returns:**\n
+    x: \n
+    y: \n
+    datax: \n
+    datay: \n
 """
 function firstpass(A, B, N, overlap, idx, idy)
-    
+    M = N[1]; N = N[2]
+    sy, sx = size(A)
 
+    xx_dim1 = ceil(Int64, ((size(A,1)-N) / ((1-overlap) * N))) + 1
+    xx_dim2 = ceil(Int64, ((size(A,2)-M) / ((1-overlap) * M))) + 1
+    xx = zeros(eltype(A), (xx_dim1, xx_dim2))
+
+    # Some pretty strange code here, but I didn't want to change it just in case
+    yy = xx
+    datax = xx; datay = xx; IN = zeros(eltype(A), size(A))
+    println(((1-overlap) * N), " to ", sy - N + 1)
+    cj = 1
+    for jj in 1:((1-overlap) * N):sy - N + 1
+        # Left off here, range function looks good, weird values though.
+        # Same as matlab so no worries.
+    end
+
+
+    # Dummy values
+    x=0; y=0;
+    return x, y, datax, datay
 end
-
 
 
 
@@ -58,21 +101,21 @@ end
 # UTILITIES
 """
 ### xcorrf2
-Two-dimensional cross-correlation using Fourier transforms.
-XCORRF2(A,B) computes the crosscorrelation of matrices A and B.
-\n**:params:**\n
-A: matrix (2D array) to be compared.\n
-B: matrix ((2D array)) to be compared.\n
-pad: Transform and trim result to optimize the speed of the FFTs. This was faster\
- in matlab. Julia does not handling padding automatically and so required two \
- O(2n) actions to be taken. Default val is false. 
-\n**:return:**\n
-c: A matrix whose values reflect the 2D correlation between every cell \
-in A & B. Matrix is 2D float 64.
+    Two-dimensional cross-correlation using Fourier transforms.
+    XCORRF2(A,B) computes the crosscorrelation of matrices A and B.
+    \n**:params:**\n
+    A: matrix (2D array) to be compared.\n
+    B: matrix ((2D array)) to be compared.\n
+    pad: Transform and trim result to optimize the speed of the FFTs. This was faster\
+    in matlab. Julia does not handling padding automatically and so required two \
+    O(2n) actions to be taken. Default val is false. 
+    \n**:return:**\n
+    c: A matrix whose values reflect the 2D correlation between every cell \
+    in A & B. Matrix is 2D float 64.
 
-Originally written in Matlab by,\n
-Author(s): R. Johnson\n
-Revision: 1.0   Date: 1995/11/27
+    Originally written in Matlab by,\n
+    Author(s): R. Johnson\n
+    Revision: 1.0   Date: 1995/11/27
 """
 function xcorrf2(A, B, pad=false)
     # Unpack size() return tuple into appropriate variables
@@ -125,17 +168,19 @@ end
 
 """
 ### Main Entry
-Minimal PIV calculation for testing and development.\n
-Run two images through the PIV process, eventually ending in the expected PIV \
-plots.\n
-This should be the only public facing function in this library.\n
-\n**:params:**\n
-A: Matrix containing image data of first frame.\n
-B: Matrix containing image data of second frame.\n
-\n**:returns:**\n
-PIV plots.
+    Minimal PIV calculation for testing and development.\n
+    Run two images through the PIV process, eventually ending in the expected PIV \
+    plots.\n
+    This should be the only public facing function in this library.\n
+    \n**:params:**\n
+    A: Matrix containing image data of first frame.\n
+    B: Matrix containing image data of second frame.\n
+    \n**:returns:**\n
+    PIV plots.
 """
 function main(A, B)
+    println("===============\nBegin execution\n===============")
+
     pivwin = 16
     log2pivwin = log2(pivwin)
     if log2pivwin - round(log2pivwin) != 0
@@ -150,30 +195,54 @@ function main(A, B)
     dt = 1; overlap = 0.5; validvec = 3
     x, y, u, v, SnR, Pkh = multipassx(A, B, pass_sizes, dt, overlap, validvec)
 
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # !!!!!!!!!! Profile stuff goes here !!!!!!!!!!!!!
-    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    
-
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # !!!!!Profile stuff goes here and rest of main() lol !!!!!
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    println("===============\nExiting now\n===============")
 end
 
 
 
 # ------ TEST ZONE ------
 
-A = [1 2 3 4 5;
-16 2 3 13 2;
-5 11 10 8 9;  
-9 7 6 12 4;
-4 14 15 1 7
+# A = [1 2 3 4 5;
+# 16 2 3 13 2;
+# 5 11 10 8 9;  
+# 9 7 6 12 4;
+# 4 14 15 1 7
+# ]
+
+A = [
+    38 28 14 42 7 20 38 18 22 10;
+    10 23 35 39 23 2 21 1 23 43;
+    29 37 1 20 32 11 21 43 24 48;
+    26 41 27 15 14 46 50 43 2 36;
+    50 6 20 8 38 17 3 24 13 49;
+    8 25 1 19 27 46 6 43 7 46;
+    34 13 16 35 49 39 3 1 5 41;
+    3 28 17 25 43 33 9 35 13 30;
+    47 14  7 13 22 39 20 15 44 17;
+    46 23 25 24 44 40 28 14 44  0
 ]
 
-B = [3 4 5 6 7;
-8 9 10 11 12;
-13 14 15 16 17;
-18 19 20 21 22;
-23 24 25 26 27
+# B = [3 4 5 6 7;
+# 8 9 10 11 12;
+# 13 14 15 16 17;
+# 18 19 20 21 22;
+# 23 24 25 26 27
+# ]
+
+B = [
+    46 34 22 49 7 27 45 28 24 10;
+    17 25 37 39 33 6 30 7 32 51;
+    35 45  8 21 32 17 27 50 28 50;
+    33 46 37 17 14 48 54 45 2 40;
+    59 12 26 18 46 26 12 26 19 49;
+    11 28  5 25 33 56 9 49 17 48;
+    39 14 25 43 53 44 6 11 14 47;
+    11 34 17 25 51 43 17 38 21 32;
+    53 19 14 23 30 43 20 17 53 24;
+    56 28 32 32 47 40 28 23 47 6
 ]
 
 main(A, B)
--- scratch/scratch.m
+++ scratch/scratch.m
@@ -4,6 +4,7 @@ function [xx,yy,datax,datay]=firstpass(A,B,N,overlap,idx,idy)
 
   M=N(1); N=N(2); 
   [sy,sx]=size(A);
+
   xx=zeros(ceil((size(A,1)-N)/((1-overlap)*N))+1, ...
       ceil((size(A,2)-M)/((1-overlap)*M)) +1);
   yy=xx;
@@ -13,147 +14,173 @@ function [xx,yy,datax,datay]=firstpass(A,B,N,overlap,idx,idy)
   
   cj=1;
   for jj=1:((1-overlap)*N):sy-N+1
-    ci=1;
-    for ii=1:((1-overlap)*M):sx-M+1 
+    disp(jj);
+  %   ci=1;
+  %   for ii=1:((1-overlap)*M):sx-M+1 
   
-      if IN(jj+N/2,ii+M/2)~=1 
+  %     if IN(jj+N/2,ii+M/2)~=1 
         
-        if isnan(idx(cj,ci))
-          idx(cj,ci)=0;
-        end
-        if isnan(idy(cj,ci))
-          idy(cj,ci)=0;
-        end
-        if jj+idy(cj,ci)<1
-          idy(cj,ci)=1-jj;
-        elseif jj+idy(cj,ci)>sy-N+1
-          idy(cj,ci)=sy-N+1-jj;
-        end       
-        if ii+idx(cj,ci)<1
-          idx(cj,ci)=1-ii;    
-        elseif ii+idx(cj,ci)>sx-M+1
-          idx(cj,ci)=sx-M+1-ii;
-        end
+  %       if isnan(idx(cj,ci))
+  %         idx(cj,ci)=0;
+  %       end
+  %       if isnan(idy(cj,ci))
+  %         idy(cj,ci)=0;
+  %       end
+  %       if jj+idy(cj,ci)<1
+  %         idy(cj,ci)=1-jj;
+  %       elseif jj+idy(cj,ci)>sy-N+1
+  %         idy(cj,ci)=sy-N+1-jj;
+  %       end       
+  %       if ii+idx(cj,ci)<1
+  %         idx(cj,ci)=1-ii;    
+  %       elseif ii+idx(cj,ci)>sx-M+1
+  %         idx(cj,ci)=sx-M+1-ii;
+  %       end
   
-        C=A(jj:jj+N-1,ii:ii+M-1);   
-        D=B(jj+idy(cj,ci):jj+N-1+idy(cj,ci),ii+idx(cj,ci):ii+M-1+idx(cj,ci));
-        C=C-mean(C(:)); D=D-mean(D(:)); %C(C<0)=0; D(D<0)=0;
-        stad1=std(C(:)); stad2=std(D(:)); 
-        %C=C.*W; %D=D.*W;  % Apply weight function by uncommenting
+  %       C=A(jj:jj+N-1,ii:ii+M-1);   
+  %       D=B(jj+idy(cj,ci):jj+N-1+idy(cj,ci),ii+idx(cj,ci):ii+M-1+idx(cj,ci));
+  %       C=C-mean(C(:)); D=D-mean(D(:)); %C(C<0)=0; D(D<0)=0;
+  %       stad1=std(C(:)); stad2=std(D(:)); 
+  %       %C=C.*W; %D=D.*W;  % Apply weight function by uncommenting
   
-        if stad1==0, stad1=nan; end
-        if stad2==0, stad2=nan; end
+  %       if stad1==0, stad1=nan; end
+  %       if stad2==0, stad2=nan; end
   
-        % normalized correlation
-        R=xcorrf2(C,D)/(N*M*stad1*stad2);
+  %       % normalized correlation
+  %       R=xcorrf2(C,D)/(N*M*stad1*stad2);
   
-        % find position of maximal value of R
-        if size(R,1)==(N-1)
-          [max_y1,max_x1]=find(R==max(R(:)));
-        else
-          [max_y1,max_x1]=find(R==max(max(R(0.5*N+2:1.5*N-3,0.5*M+2:1.5*M-3))));
-        end
+  %       % find position of maximal value of R
+  %       if size(R,1)==(N-1)
+  %         [max_y1,max_x1]=find(R==max(R(:)));
+  %       else
+  %         [max_y1,max_x1]=find(R==max(max(R(0.5*N+2:1.5*N-3,0.5*M+2:1.5*M-3))));
+  %       end
         
-        if length(max_x1)>1
-          max_x1=round(sum(max_x1.*(1:length(max_x1))')./sum(max_x1));
-          max_y1=round(sum(max_y1.*(1:length(max_y1))')./sum(max_y1));
-        elseif isempty(max_x1)
-          idx(cj,ci)=nan;
-          idy(cj,ci)=nan;
-          max_x1=nan;
-          max_y1=nan;
-        end
+  %       if length(max_x1)>1
+  %         max_x1=round(sum(max_x1.*(1:length(max_x1))')./sum(max_x1));
+  %         max_y1=round(sum(max_y1.*(1:length(max_y1))')./sum(max_y1));
+  %       elseif isempty(max_x1)
+  %         idx(cj,ci)=nan;
+  %         idy(cj,ci)=nan;
+  %         max_x1=nan;
+  %         max_y1=nan;
+  %       end
   
-        % store the displacements in variable datax/datay
-        datax(cj,ci)=-(max_x1-(M))+idx(cj,ci);
-        datay(cj,ci)=-(max_y1-(N))+idy(cj,ci);
-        xx(cj,ci)=ii+M/2; yy(cj,ci)=jj+N/2;
-        ci=ci+1;
-      else
-        xx(cj,ci)=ii+M/2; yy(cj,ci)=jj+N/2;
-        datax(cj,ci)=NaN; datay(cj,ci)=NaN; ci=ci+1;
-      end  
-    end
+  %       % store the displacements in variable datax/datay
+  %       datax(cj,ci)=-(max_x1-(M))+idx(cj,ci);
+  %       datay(cj,ci)=-(max_y1-(N))+idy(cj,ci);
+  %       xx(cj,ci)=ii+M/2; yy(cj,ci)=jj+N/2;
+  %       ci=ci+1;
+  %     else
+  %       xx(cj,ci)=ii+M/2; yy(cj,ci)=jj+N/2;
+  %       datax(cj,ci)=NaN; datay(cj,ci)=NaN; ci=ci+1;
+      % end  
+    % end
   
-    cj=cj+1;
+  %   cj=cj+1;
   end
 end
 
 
 function c = xcorrf2(a,b,pad)
-if nargin==2
-  pad='yes';
-end
+  if nargin==2
+    pad='yes';
+  end
 
-[ma,na] = size(a);
-[mb,nb] = size(b);
+  [ma,na] = size(a);
+  [mb,nb] = size(b);
 
-% make reverse conjugate of one array
-b = conj(b(mb:-1:1,nb:-1:1));
-if strcmp(pad,'yes')   % use power of 2 transform lengths
-  mf = 2^nextpow2(ma+mb);
-  nf = 2^nextpow2(na+nb);
-  at = fft2(b,mf,nf);
-  bt = fft2(a,mf,nf);
-elseif strcmp(pad,'no')
-  at = fft2(b);
-  bt = fft2(a);
-else
-  error('Wrong input to XCORRF2');
-end
+  % make reverse conjugate of one array
+  b = conj(b(mb:-1:1,nb:-1:1));
+  if strcmp(pad,'yes')   % use power of 2 transform lengths
+    mf = 2^nextpow2(ma+mb);
+    nf = 2^nextpow2(na+nb);
+    at = fft2(b,mf,nf);
+    bt = fft2(a,mf,nf);
+  elseif strcmp(pad,'no')
+    at = fft2(b);
+    bt = fft2(a);
+  else
+    error('Wrong input to XCORRF2');
+  end
 
-% multiply transforms then inverse transform
-c = ifft2(at.*bt);
+  % multiply transforms then inverse transform
+  c = ifft2(at.*bt);
 
-% make real output for real input
-if ~any(any(imag(a))) && ~any(any(imag(b)))
-  c = real(c);
-end
+  % make real output for real input
+  if ~any(any(imag(a))) && ~any(any(imag(b)))
+    c = real(c);
+  end
 
-if strcmp(pad,'yes');    % trim to standard size
-  c(ma+mb:mf,:) = [];
-  c(:,na+nb:nf) = [];
-elseif strcmp(pad,'no');
-  c=(c(1:end-1,1:end-1));
-end
+  if strcmp(pad,'yes');    % trim to standard size
+    c(ma+mb:mf,:) = [];
+    c(:,na+nb:nf) = [];
+  elseif strcmp(pad,'no');
+    c=(c(1:end-1,1:end-1));
+  end
 end
 
 
 % ------ TEST ZONE ------
-A = [1 2 3 4 5;
-  16 2 3 13 2;
-  5 11 10 8 9;
-  9 7 6 12 4;
-  4 14 15 1 7
-  ];
+% A = [1 2 3 4 5;
+%   16 2 3 13 2;
+%   5 11 10 8 9;
+%   9 7 6 12 4;
+%   4 14 15 1 7
+%   ];
 
-B = [3 4 5 6 7;
-  8 9 10 11 12;
-  13 14 15 16 17;
-  18 19 20 21 22;
-  23 24 25 26 27
-  ];
+A = [
+    38, 28, 14, 42, 7, 20, 38, 18, 22, 10;
+    10, 23, 35, 39, 23, 2, 21, 1, 23, 43;
+    29, 37, 1, 20, 32, 11, 21, 43, 24, 48;
+    26, 41, 27, 15, 14, 46, 50, 43, 2, 36;
+    50, 6, 20, 8, 38, 17, 3, 24, 13, 49;
+    8, 25, 1, 19, 27, 46, 6, 43, 7, 46;
+    34, 13, 16, 35, 49, 39, 3, 1, 5, 41;
+    3, 28, 17, 25, 43, 33, 9, 35, 13, 30;
+    47, 14, 7, 13, 22, 39, 20, 15, 44, 17;
+    46, 23, 25, 24, 44, 40, 28, 14, 44, 0
+];
 
-pivwin=16;  % 16pix is a final window size that worked well for ROXSI 2023
+% B = [3 4 5 6 7;
+%   8 9 10 11 12;
+%   13 14 15 16 17;
+%   18 19 20 21 22;
+%   23 24 25 26 27
+%   ];
+
+B = [
+    46, 34, 22, 49, 7, 27, 45, 28, 24, 10;
+    17, 25, 37, 39, 33, 6, 30, 7, 32, 51;
+    35, 45, 8, 21, 32, 17, 27, 50, 28, 50;
+    33, 46, 37, 17, 14, 48, 54, 45, 2, 40;
+    59, 12, 26, 18, 46, 26, 12, 26, 19, 49;
+    11, 28, 5, 25, 33, 56, 9, 49, 17, 48;
+    39, 14, 25, 43, 53, 44, 6, 11, 14, 47;
+    11, 34, 17, 25, 51, 43, 17, 38, 21, 32;
+    53, 19, 14, 23, 30, 43, 20, 17, 53, 24;
+    56, 28, 32, 32, 47, 40, 28, 23, 47, 6
+];
+
+% JUST SET UP FOR FIRSTPASS
+% ------------------------------------------------------------------------------
+pivwin=16;
 log2pivwin=log2(pivwin);
-if(log2pivwin-round(log2pivwin)~=0)
-  error('pivwin must be a factor of 2')
-end
-pass_sizes=2.^[6:-1:log2pivwin]';  % step-down the window sizes in powers of two
-
-% make the analysis window sizes 2d (as required for the matpiv code), and
-% repeat the last window iteration (also required)
+pass_sizes=2.^[6:-1:log2pivwin]';
 pass_sizes=[pass_sizes pass_sizes];
 pass_sizes=[pass_sizes;pass_sizes(end,:)];
-wins = pass_sizes;
+wins = pass_sizes; 
 dt = 1; overlap = 0.5; validvec = 3;
-
 A=double(A);
 B=double(B);
 [sy,sx]=size(A);
 iter=size(wins,1);
+datax=zeros(floor(sy/(wins(1,1)*(1-overlap))),floor(sx/(wins(1,2)*(1-overlap))));
+datay=zeros(floor(sy/(wins(1,1)*(1-overlap))),floor(sx/(wins(1,2)*(1-overlap))));
+% ------------------------------------------------------------------------------
 
+for i=1:iter-1
+  firstpass(A, B, wins(i, :), overlap, datax, datay)
+end
 
-% datax=zeros(floor(sy/(wins(1,1)*(1-overlap))),floor(sx/(wins(1,2)*(1-overlap))));
-% floor(sy/(wins(1,1)*(1-overlap)))
 % ------ TEST ZONE ------
