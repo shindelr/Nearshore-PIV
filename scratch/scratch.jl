d3f04464014175e543ea4a1a99a4cd2394715fcb
Robin Shindelman <shindelr@aquilla.ceoas.oregonstate.edu>
2024-10-10 10:11:42 -0700

    Got a working GPU version of localfilt going. Trying to figure out how to plug it into the main program now.

--- scratch/scratch.jl
+++ scratch/scratch.jl
@@ -1,12 +1,13 @@
 using CUDA
 using StaticArrays
+using DelimitedFiles
 
-# function test_kernel!(U2, histo, histostd)
-function test_kernel!(U2)
+function test_kernel!(U2, histo, histostd)
     i = threadIdx().x + (blockIdx().x - Int32(1)) * blockDim().x
     j = threadIdx().y + (blockIdx().y - Int32(1)) * blockDim().y
     
-    subarray = @MVector fill(ComplexF32(NaN + NaN * im), 9)
+    # subarray = @MVector fill(ComplexF32(NaN + NaN * im), 9)
+    subarray = @MVector zeros(ComplexF32, 9)
 
     if i > 1 && i < size(U2, 1) && j > 1 && j < size(U2, 2)
 
@@ -33,7 +34,7 @@ function test_kernel!(U2)
             end
         end
         if not_nan_count == 0
-            # TODO: Make histo[j, i] = NaN
+            # histo[j, i] = NaN Might not be any need because it's already NaN
             return
         end
 
@@ -51,12 +52,6 @@ function test_kernel!(U2)
             end
         end
 
-        if threadIdx().x == 2 && threadIdx().y == 2 && blockIdx().x == 1 && blockIdx().y == 1
-            for val in valid_vals
-                @cuprintln(real(val))
-            end
-        end
-
         # 4) Get median value
         mid = div(not_nan_count, 2)
         if not_nan_count % 2 == 0
@@ -65,33 +60,56 @@ function test_kernel!(U2)
             median = valid_vals[mid + 1]
         end
 
-        if threadIdx().x == 2 && threadIdx().y == 2 && blockIdx().x == 1 && blockIdx().y == 1
-            @cushow real(median)
+        # 5) Get standard deviation
+        real_mean = 0
+        im_mean = 0
+        real_var = 0
+        im_var = 0
+        for k in 1:not_nan_count
+            real_mean += real(valid_vals[k])
+            im_mean += imag(valid_vals[k])
         end
+        real_mean /= not_nan_count
+        im_mean /= not_nan_count
+
+        for k in 1:not_nan_count
+            real_var += (valid_vals[k] - real_mean)^2
+            im_var += (valid_vals[k] - real_mean)^2
+        end
+        real_std = sqrt(real_var / not_nan_count)
+        im_std = sqrt(im_var / not_nan_count)
+
+        std = real_std + im_std * im
+
+        # 6) Set histo[j, i] and histostd[j, i]
+        histo[i, j] = median
+        histostd[i, j] = std
 
     end
     return
 end
 
 # function bencher(U2, histo, histostd)
-function bencher()
+function bencher(U2)
 
     # Test U2 matrix
-    U2 = @SMatrix ComplexF32[
-           3 + 4im 1 + 2im 7 + 8im 0 + 0im 5 + 6im;
-           9 + 10im 11 - 12im 15 - 16im 13 + 14im 7 - 8im;
-           21 + 22im 17 + 18im 19 - 20im 25 + 26im 15 - 16im;
-           27 - 28im 23 - 24im 29 + 30im 31 - 32im 25 + 26im
-       ]
-
+    # U2 = @SMatrix ComplexF32[
+    #        3 + 4im 1 + 2im 7 + 8im 0 + 0im 5 + 6im;
+    #        9 + 10im 11 - 12im 15 - 16im 13 + 14im 7 - 8im;
+    #        21 + 22im 17 + 18im 19 - 20im 25 + 26im 15 - 16im;
+    #        27 - 28im 23 - 24im 29 + 30im 31 - 32im 25 + 26im
+    #    ]
+    
+    histo = CUDA.zeros(ComplexF32, size(U2, 1), size(U2, 2))
+    histostd = CUDA.zeros(ComplexF32, size(U2, 1), size(U2, 2))
+       
     # 2D array size
     M = size(U2, 1)
     N = size(U2, 2)
 
 
     # Get config for kernel
-    # kernel = @cuda launch=false test_kernel!(U2, histo, histostd)
-    kernel = @cuda launch=false test_kernel!(U2)
+    kernel = @cuda launch=false test_kernel!(U2, histo, histostd)
     config = launch_configuration(kernel.fun)
 
     # Specificy nukber of threads and blocks for 2D array
@@ -101,11 +119,83 @@ function bencher()
     blocks_y = cld(N, threads_y)
     
     CUDA.@sync begin
-        # kernel(U2, histo, histostd; 
-        kernel(U2; 
+        kernel(U2, histo, histostd; 
                threads=(threads_x, threads_y), blocks=(blocks_x, blocks_y)
             )
     end
-
     return
+end
+
+function localfilt(U2)
+    # U2 = @SMatrix ComplexF32[
+    #     3 + 4im 1 + 2im 7 + 8im 0 + 0im 5 + 6im;
+    #     9 + 10im 11 - 12im 15 - 16im 13 + 14im 7 - 8im;
+    #     21 + 22im 17 + 18im 19 - 20im 25 + 26im 15 - 16im;
+    #     27 - 28im 23 - 24im 29 + 30im 31 - 32im 25 + 26im
+    # ]
+ 
+    histo = zeros(ComplexF32, size(U2, 1), size(U2, 2))
+    histostd = zeros(ComplexF32, size(U2, 1), size(U2, 2))
+
+    ma, na = size(U2)
+    m = 3
+
+    for ii in m-1:1:na-m+2
+        for jj in m-1:1:ma-m+2
+            # Get a 3x3 submatrix of U2
+            m_floor_two = floor(Int32, m / 2)
+            tmp = U2[jj-m_floor_two:jj+m_floor_two,
+                        ii-m_floor_two:ii+m_floor_two]
+
+            # Assign the center value to NaN
+            tmp[ceil(Int32, m / 2), ceil(Int32, m / 2)] = NaN
+
+            # Run the appropriate stat depending on method arg.
+            histo[jj, ii] = im_median_magnitude(tmp[:])
+            histostd[jj, ii] = im_std(tmp[:])
+        end
+    end  
+end
+
+function im_median_magnitude(collection)
+    i = filter(x -> !isnan(x), collection)
+    isempty(i) && return NaN
+    n = length(i)
+    v = partialsort!(i, 
+                    div(n + 1, 2, RoundDown):div(n + 1, 2, RoundUp); 
+                    by=x -> (abs2(x), angle(x))
+                )
+    return sum(v) / length(v)
+end
+
+function im_std(collection)
+    i = filter(x -> !isnan(x), collection)
+
+    if length(i) > 0
+        real_part = std(real(i), corrected=false)
+        im_part = std(imag(i), corrected=false)
+        return real_part + im_part * im
+    end
+
+    # All NaN!
+    return NaN
+end
+
+function build_cu_u2()
+    nv = readdlm("../tests/gpu_tests/nv.csv", ',')
+    nu = readdlm("../tests/gpu_tests/nu.csv", ',')
+
+    nv_cu = cu(nv)
+    nu_cu = cu(nu)
+
+    U2 = nu_cu .+ im .* nv_cu
+    bencher(U2)
+
+end
+
+function build_normal_u2()
+    nv = readdlm("../tests/gpu_tests/nv.csv", ',')
+    nu = readdlm("../tests/gpu_tests/nu.csv", ',')
+
+    U2::Matrix{ComplexF32} = nu .+ im .* nv
 end
