26bb28d9b479fb8bfa57ab0a5a98ff1bb15feac7
shindelr <129821483+shindelr@users.noreply.github.com>
2025-03-17 12:51:38 -0400

    Executable too large to be stored on vanilla github. Just pushing multiprocessing changes to the pipeline.

--- piv_pipeline.py
+++ piv_pipeline.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/Users/robinshindelman/repos/Nearshore-Research/.venv/bin/python3
 """
 A script to run the Julia PIV portion of the pipeline.
 Uses 4 threads by default to make the comparison of frame pairs run a little 
@@ -9,11 +9,25 @@ Meant to be executed from the top level of the juliaPIV directory.
 import os
 import argparse
 import subprocess
+import logging
+from multiprocessing import Pool
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s -- %(levelname)s -- %(message)s")
+NPROC = 4
+
+def launch_batch(file, args):
+    args.in_path = file
+    logging.info(f"Processing {args.in_path}")
+    run_pipe(args=args)
+
+
+def batches(abs_batch_dir):
+    return [os.path.join(abs_batch_dir, f) for f in os.listdir(abs_batch_dir)]
+
 
 def run_pipe(args):
     # Nifty way of getting absolute path of the script to run, regardless of cwd
     exec_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'piv_build/bin/PIVPipelineUtility'))
-    print(exec_path)
     cmmd = [exec_path,
             str(args.N),
             str(args.crop_factor), 
@@ -23,6 +37,7 @@ def run_pipe(args):
             args.in_path, 
             str(args.verbosity)]
     subprocess.run(cmmd)
+
 
 def main():
     output_mat_structure = """
@@ -45,25 +60,32 @@ pass_sizes: [3x2 double]
     parser = argparse.ArgumentParser(prog='PIV Pipeline Utility',
                                      description='Run Julia PIV on a batch of frames.',
                                      epilog=output_mat_structure)
+    
     parser.add_argument('-N', 
                         help="The number of frames to average together at once.",
                         type=int,
                         default=2)
+    
     parser.add_argument('--crop_factor',
                         help='Gives a box to extract from the raw image. Should be a tuple of 4 ints',
                         default="24, 2425, 1, 2048")
+    
     parser.add_argument('--final_win_size',
                         help='Final window size to evaluate PIV at.',
                         type=int,
                         default=16)
+    
     parser.add_argument('--ol', 
                         help='Window overlap for frame comparison.',
                         type=float,
                         default=0.5)
+    
     parser.add_argument('--out',
                         help='Where to output .mat files')
+    
     parser.add_argument('--in_path',
                         help='.txt file containing image paths.')
+    
     parser.add_argument('--verbosity', 
                         help='1 for verbose print statements, 0 otherwise.',
                         type=int,
@@ -71,7 +93,12 @@ pass_sizes: [3x2 double]
 
     args = parser.parse_args()
 
-    run_pipe(args=args)
+    txt_list = batches(args.in_path)
+    logging.info(f"Found {len(txt_list)} .txt files\n")
+
+    with Pool(processes=NPROC) as pool:
+        pool.starmap(launch_batch, [(file, args) for file in txt_list])
+
 
 if __name__ == '__main__':
     main()
